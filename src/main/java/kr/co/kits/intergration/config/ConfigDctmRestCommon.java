8489a73adc572b1097d1eda3abe487f8e6ecd121
Lee Hak Do <dev.fringe@gmail.com>
2020-06-30 16:43:36 +0900

    startup loading error resolve

--- src/main/java/kr/co/kits/intergration/config/ConfigDctmRestCommon.java
+++ src/main/java/kr/co/kits/intergration/config/ConfigDctmRestCommon.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.AbstractEnvironment;
